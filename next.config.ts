a3eb9397a542de545f90ec28ba4f0e9c495c6a70
Amit <beamitpal@gmail.com>
2025-11-19 11:31:13 +0530

    FIX:Dynamic fetch 4

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,20 @@ const nextConfig: NextConfig = {
     ]
   },
   async rewrites() {
-    return [
-      { source: '/about.md', destination: '/about-md' },
-      { source: '/awards.md', destination: '/awards-md' },
-      { source: '/blog.mdx/:path*', destination: '/blog-mdx/:path*' },
-      { source: '/certifications.md', destination: '/certifications-md' },
-      { source: '/experience.md', destination: '/experience-md' },
-      { source: '/llms-full.txt', destination: '/llms-full-txt' },
-      { source: '/llms.txt', destination: '/llms-txt' },
-      { source: '/projects.md', destination: '/projects-md' },
-    ];
+    return {
+      beforeFiles: [
+        { source: '/about.md', destination: '/about-md' },
+        { source: '/awards.md', destination: '/awards-md' },
+        { source: '/blog.mdx/:path*', destination: '/blog-mdx/:path*' },
+        { source: '/certifications.md', destination: '/certifications-md' },
+        { source: '/experience.md', destination: '/experience-md' },
+        { source: '/llms-full.txt', destination: '/llms-full-txt' },
+        { source: '/llms.txt', destination: '/llms-txt' },
+        { source: '/projects.md', destination: '/projects-md' },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
   },
 };
 
